af14ebaff7da763e00f3bbcc0cccf5ec45c80130
NIKESH <nikeshnoida@gmail.com>
2026-01-21 21:45:28 +0530

    Update 13object.js

--- 13objects/13object.js
+++ 13objects/13object.js
@@ -1,1 +1,10 @@
-//chai aur code objects vids and mdn 
+/*object in js core data structure for real world entities
+  object stores value in key-value pair
+  they are flexible, dynamic and deeply tied to the language prototype system  */
+
+
+
+
+
+
+  
