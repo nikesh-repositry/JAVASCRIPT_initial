2c8ee6c247021c73aebfc1b00845ebbc6fcd3cbd
NIKESH <nikeshnoida@gmail.com>
2026-02-15 18:37:16 +0530

    Update 20api.js

--- 20API/20api.js
+++ 20API/20api.js
@@ -1,1 +1,13 @@
 //                              API
+
+async function fetchData() {
+    // 1. Send the request and wait for the response
+    const response = await fetch('https://api.example.com/data');
+    
+    // 2. Convert the response into usable JSON format
+    const data = await response.json();
+    
+    // 3. Use the data
+    console.log(data);
+}
+fetchData();
